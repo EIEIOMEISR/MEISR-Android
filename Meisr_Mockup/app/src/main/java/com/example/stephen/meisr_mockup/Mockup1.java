cb0c405f561b4462ca1c901b63b99c980878abba
Stephen <sabowles@crimson.ua.edu>
2018-02-15 17:34:43 -0600

    Created a project to begin work on a mockup app for concept presentation.
    Created first draft of login screen. Not completed yet.

---
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/Mockup1.java
@@ -0,0 +1,15 @@
+package com.example.stephen.meisr_mockup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mockup1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mockup1);
+    }
+
+
+}
