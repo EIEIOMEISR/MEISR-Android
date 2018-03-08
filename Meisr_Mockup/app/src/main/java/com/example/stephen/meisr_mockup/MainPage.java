21ce75ab2f47249e101a7cd0bd38fc48e3a536b4
Kevin Ziegler <kziegler@crimson.ua.edu>
2018-03-08 17:41:29 -0600

    We added non-working survey template

--- Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/MainPage.java
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/MainPage.java
@@ -5,17 +5,31 @@ package com.example.stephen.meisr_mockup;
  */
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainPage  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        final Button newsurvey = findViewById(R.id.newsurvey);
+        newsurvey.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Code here executes on main thread after user presses button
+                    Intent nextScreen = new Intent(view.getContext(), NewSurvey.class);
+                    startActivityForResult(nextScreen, 0);
+
+
+                //query login information from database
+            }
+        });
 
 
     }
---
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/NewSurvey.java
@@ -0,0 +1,23 @@
+package com.example.stephen.meisr_mockup;
+
+/**
+ * Created by kevin on 3/8/2018.
+ */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NewSurvey  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_survey);
+
+
+    }
+
+}
