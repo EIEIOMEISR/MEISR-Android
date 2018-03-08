9f327681da2f833eeab1622b059e0724dc8840f1
Kevin Ziegler <kziegler@crimson.ua.edu>
2018-03-08 14:37:20 -0600

    Click Listener prints user name and pass. I want to test if pushing works

--- Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/Mockup1.java
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/Mockup1.java
@@ -2,6 +2,10 @@ package com.example.stephen.meisr_mockup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Mockup1 extends AppCompatActivity {
 
@@ -9,6 +13,29 @@ public class Mockup1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mockup1);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Log.d("myTag", "This is my message");
+                TextView editText = findViewById(R.id.editText);
+                TextView editText2 = findViewById(R.id.editText2);
+
+
+                String login = editText.getText().toString();
+                String password = editText2.getText().toString();
+
+                Log.d("Login is", login);
+                Log.d("Password is", password);
+
+
+
+
+                //query login information from database
+            }
+        });
+
     }
 
 
