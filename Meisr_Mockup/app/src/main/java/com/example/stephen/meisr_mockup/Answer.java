04fd08b8bd0af9a14d9344059c539eac704d3f79
Kevin Ziegler <kziegler@crimson.ua.edu>
2018-03-29 14:28:24 -0500

    This is the same

---
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/Answer.java
@@ -0,0 +1,8 @@
+package com.example.stephen.meisr_mockup;
+
+/**
+ * Created by kevin on 3/29/2018.
+ */
+
+public class Answer {
+}
---
+++ Meisr_Mockup/app/src/main/java/com/example/stephen/meisr_mockup/Survey.java
@@ -0,0 +1,25 @@
+package com.example.stephen.meisr_mockup;
+
+import org.json.JSONArray;
+
+/**
+ * Created by kevin on 3/29/2018.
+ */
+
+public class Survey {
+    //JSONArray listQuestions;
+    String test;
+
+    /*public JSONArray getQuestions(){
+        return listQuestions;
+    }*/
+
+    public void setQuestions(String y) {
+       // listQuestions = x;
+        test = y;
+    }
+
+    public String getString(){
+        return test;
+    }
+}
